4d461d9cd3d08e75f1f3f27b49599c2eec9cec11
Advay Patil <advaypatil27@gmail.com>
2024-10-12 16:58:58 -0700

    mtth

--- Assets/Scripts/NextButton.ts
+++ Assets/Scripts/NextButton.ts
@@ -3,13 +3,22 @@ import { PinchButton } from "SpectaclesInteractionKit/Components/UI/PinchButton/
 @component
 export class NewScript extends BaseScriptComponent {
   @input pinchButton: PinchButton;
+  @input isBack: boolean;
+  @input stepText: Text;
 
   onAwake() {
     // Register Listener
-    this.pinchButton.onButtonPinched.add(this.onButtonPressed);
+    this.pinchButton.onButtonPinched.add(() => {
+      this.onButtonPressed();
+    });
   }
 
   onButtonPressed() {
-    print("button pressed");
+    let stepNumber = parseInt(
+      this.stepText.text[this.stepText.text.length - 1]
+    );
+    stepNumber += this.isBack ? -1 : 1;
+    stepNumber = Math.max(stepNumber, 0);
+    this.stepText.text = `Step #${stepNumber}`;
   }
 }
