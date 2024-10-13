b55008420a5462d49a553554f80d052b81579f0d
Advay Patil <advaypatil27@gmail.com>
2024-10-12 18:33:57 -0700

    Add more functionality to the timer

--- Assets/Scripts/Timer.ts
+++ Assets/Scripts/Timer.ts
@@ -1,16 +1,59 @@
+import { PinchButton } from "SpectaclesInteractionKit/Components/UI/PinchButton/PinchButton";
+
 @component
 export class Timer extends BaseScriptComponent {
   @input timerLength: number;
   timerText: Text;
   private currentValue: number;
+  private timeMultiplier: number;
+
+  @input increaseTimeButton: PinchButton;
+  @input decreaseTimeButton: PinchButton;
+  @input startButton: PinchButton;
+  @input stopButton: PinchButton;
 
   onAwake() {
+    this.timeMultiplier = 1;
+
     this.currentValue = this.timerLength;
     this.createEvent("UpdateEvent").bind(() => {
       this.onUpdate();
     });
 
     this.timerText = this.getSceneObject().getComponent("Text");
+
+    this.increaseTimeButton.onButtonPinched.add(() => {
+      this.increaseTime();
+    });
+
+    this.decreaseTimeButton.onButtonPinched.add(() => {
+      this.decreaseTime();
+    });
+
+    this.startButton.onButtonPinched.add(() => {
+      this.startTime();
+    });
+
+    this.stopButton.onButtonPinched.add(() => {
+      this.stopTime();
+    });
+  }
+
+  increaseTime() {
+    this.currentValue += 60;
+  }
+
+  decreaseTime() {
+    this.currentValue -= 60;
+    this.currentValue = Math.max(0, this.currentValue);
+  }
+
+  stopTime() {
+    this.timeMultiplier = 0;
+  }
+
+  startTime() {
+    this.timeMultiplier = 1;
   }
 
   getMinutes() {
@@ -34,6 +77,6 @@ export class Timer extends BaseScriptComponent {
     }
     this.timerText.text = `${this.getMinutes()}:${this.getSeconds()}`;
 
-    this.currentValue -= getDeltaTime();
+    this.currentValue -= getDeltaTime() * this.timeMultiplier;
   }
 }
